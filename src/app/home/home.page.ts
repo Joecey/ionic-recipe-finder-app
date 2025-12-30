5f6104a04f74f9c51257829e080931c2b49925c1
Joecey <joe.linogao@gmail.com>
2025-12-30 11:13:42 +0000

    added services and search bar working

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -5,20 +5,46 @@ import {
     IonTitle,
     IonContent,
     IonSearchbar,
+    IonButton,
 } from '@ionic/angular/standalone'
 import { GlobalMenuComponent } from '../components/global-menu/global-menu.component'
+import { FormsModule } from '@angular/forms'
 
 @Component({
     selector: 'app-home',
     templateUrl: 'home.page.html',
     styleUrls: ['home.page.scss'],
     standalone: true,
-    imports: [IonHeader, IonToolbar, IonTitle, IonContent, IonSearchbar, GlobalMenuComponent],
+    imports: [
+        IonHeader,
+        IonToolbar,
+        IonTitle,
+        IonContent,
+        IonSearchbar,
+        IonButton,
+        GlobalMenuComponent,
+        FormsModule,
+    ],
 })
 export class HomePage {
-    constructor() {}
-
     // TODO: add service to get recipes
     // TODO: on load or if search is empty, show  random recipes
     // TODO: figure out how to navigate to recipe details with a specific recipe id
+
+    ingredientSearch: string = ''
+
+    // Function used to search recipes based on ingredientSearch
+    searchRecipes() {
+        if (!this.ingredientSearch.trim()) {
+            return
+        }
+
+        // The ingredients need to be formatted as the following (ingredients=apples,+flour,+sugar)
+        const formattedIngredients = this.ingredientSearch
+            .split(',')
+            .map((ingredient) => ingredient.trim())
+            .join(',+')
+
+        alert(`you searched for ${formattedIngredients}`)
+    }
 }
